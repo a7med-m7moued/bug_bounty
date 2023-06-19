6b4015a6b7a5f9bdc3f621b09728ad0b9e9dc8bb
Ahmed Mahmoued <116384283+a7med-m7moued@users.noreply.github.com>
2023-06-19 14:49:38 +0300

    recon

--- Recon/install.sh
+++ Recon/install.sh
@@ -115,7 +115,9 @@ wget https://github.com/tomnomnom/anew/releases/download/v0.1.1/anew-linux-amd64
 
 tar zxf anew-linux-amd64-0.1.1.tgz
 cp anew /usr/bin/
-wget https://github.com/projectdiscovery/dnsx/releases/download/v1.1.4/dnsx_1.1.4_linux_amd64.zip
-unzip dnsx_1.1.4_linux_amd64.zip 
-cp dnsx /usr/bin/
 
+wget https://github.com/projectdiscovery/dnsx/releases/download/v1.1.4/dnsx_1.1.4_linux_amd64.zip
+
+unzip dnsx_1.1.4_linux_amd64.zip 
+
+cp dnsx /usr/bin/
